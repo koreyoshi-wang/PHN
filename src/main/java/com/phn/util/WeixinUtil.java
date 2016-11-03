b3499181cee3e072a8a03423ada4c4fecc6d7325
fanggang-wang <fanggang.wang@hpe.com>
2016-11-03 16:15:22 +0800

    qwerqwer

---
+++ src/main/java/com/phn/util/WeixinUtil.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package com.phn.util;
+
+
+import java.io.InputStream;
+import java.io.Writer;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+/**
+ * @author wanfangg
+ */
+public class WeixinUtil {
+	
+	private static XStream xstream = new XStream(new XppDriver() {  
+        public HierarchicalStreamWriter createWriter(Writer out) {  
+            return new PrettyPrintWriter(out) {  
+                // 对所有xml节点的转换都增加CDATA标记  
+                boolean cdata = true;  
+                @SuppressWarnings("unchecked")  
+                public void startNode(String name, Class clazz) {  
+                    super.startNode(name, clazz);  
+                }  
+                protected void writeText(QuickWriter writer, String text) {  
+                    if (cdata) {  
+                        writer.write("<![CDATA[");  
+                        writer.write(text);  
+                        writer.write("]]>");  
+                    } else {  
+                        writer.write(text);  
+                    }  
+                }  
+            };  
+        }  
+    });  
+	
+
+	/**
+	 * 解析request中的xml 并将数据存储到一个Map中返回
+	 * @param request
+	 */
+	public static Map<String, String> parseXml(HttpServletRequest request){
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			InputStream inputStream = request.getInputStream();
+			SAXReader reader = new SAXReader();
+			Document document = reader.read(inputStream);
+			Element root = document.getRootElement();
+			List<Element> elementList = root.elements();
+			for (Element e : elementList)
+				//遍历xml将数据写入map
+				map.put(e.getName(), e.getText());
+			inputStream.close();
+			inputStream = null;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+
+	
+	/**
+	 * sha1加密算法
+	 * @param key需要加密的字符串
+	 * @return 加密后的结果
+	 */
+	public static String sha1(String key) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA1");
+			md.update(key.getBytes());
+			String pwd = new BigInteger(1, md.digest()).toString(16);
+			return pwd;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return key;
+		}
+	}
+	
+}
